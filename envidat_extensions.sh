e42af10e747a6449f4f6073574806e0f1a7dad84
sam <sam.woodcock@protonmail.com>
2021-12-24 02:17:26 +0000

    handle ckanext_spatial pip-requirements.txt

--- envidat_extensions.sh
+++ envidat_extensions.sh
@@ -9,6 +9,10 @@ while IFS=+ read -r _ repo_url; do
         echo "Installing dependencies for $repo_url"
         PIPENV_VENV_IN_PROJECT=1 pipenv run \
             python -m pip install -r "./ckanext/requirements.txt"
+    elif [ -f ./ckanext/pip-requirements.txt ]; then
+        echo "Installing dependencies for $repo_url"
+        PIPENV_VENV_IN_PROJECT=1 pipenv run \
+            python -m pip install -r "./ckanext/pip-requirements.txt"
     fi
     rm -rf ./ckanext
 done </opt/repos/envidat_extensions.txt
