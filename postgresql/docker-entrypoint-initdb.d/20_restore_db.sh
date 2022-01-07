e2fb86666cd9838d11ef9f18b8eea19f7ea95cc4
sam <sam.woodcock@protonmail.com>
2022-01-07 16:21:01 +0000

    update psql to pg_restore to prevent pipe errors

--- postgresql/docker-entrypoint-initdb.d/20_restore_db.sh
+++ postgresql/docker-entrypoint-initdb.d/20_restore_db.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 set -e
 
-PGPASSWORD="$CKAN_DB_PASS" pg_dump -C -h "$CKAN_DB_HOST" -U "$CKAN_DB_USER" "$CKAN_DB_NAME" | psql -U "$CKAN_DB_USER" "$CKAN_DB_NAME"
-PGPASSWORD="$CKAN_DB_PASS" pg_dump -C -h "$CKAN_DB_HOST" -U "$CKAN_DB_USER" "$CKAN_DOI_DB_NAME" | psql -U "$CKAN_DB_USER" "$CKAN_DOI_DB_NAME"
+PGPASSWORD="$CKAN_DB_PASS" pg_dump -C --format c\
+    --host "$CKAN_DB_HOST" --username "$CKAN_DB_USER" \
+    "$CKAN_DB_NAME" | \
+    pg_restore --exit-on-error \
+    --username postgres --dbname "$CKAN_DB_NAME"
+
+PGPASSWORD="$CKAN_DB_PASS" pg_dump -C --format c\
+    --host "$CKAN_DB_HOST" --username "$CKAN_DB_USER" \
+    "$CKAN_DOI_DB_NAME" | \
+    pg_restore --exit-on-error \
+    --username postgres --dbname "$CKAN_DOI_DB_NAME"
