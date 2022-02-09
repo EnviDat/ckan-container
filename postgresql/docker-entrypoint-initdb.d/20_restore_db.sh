19e80df169a0df875d2ef0c8a25bad938994aae2
sam <sam.woodcock@protonmail.com>
2022-02-09 13:38:54 +0000

    [#181203236] formatting + test pivotal integration

--- postgresql/docker-entrypoint-initdb.d/20_restore_db.sh
+++ postgresql/docker-entrypoint-initdb.d/20_restore_db.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 
-PGPASSWORD="$CKAN_DB_PASS" pg_dump -C --format c\
+PGPASSWORD="$CKAN_DB_PASS" pg_dump -C --format c \
     --host "$CKAN_DB_HOST" --username "$CKAN_DB_USER" \
     "$CKAN_DB_NAME" | \
     pg_restore --exit-on-error \
     --username postgres --dbname "$CKAN_DB_NAME"
 
-PGPASSWORD="$CKAN_DB_PASS" pg_dump -C --format c\
+PGPASSWORD="$CKAN_DB_PASS" pg_dump -C --format c \
     --host "$CKAN_DB_HOST" --username "$CKAN_DB_USER" \
     "$CKAN_DOI_DB_NAME" | \
     pg_restore --exit-on-error \
