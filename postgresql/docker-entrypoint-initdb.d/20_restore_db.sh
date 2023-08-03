94d5cfd0729e011d9fdb4a791a4b884b70222463
sam <sam.woodcock@protonmail.com>
2023-08-03 12:18:15 +0100

    build: remove postgis

--- postgresql/docker-entrypoint-initdb.d/20_restore_db.sh
+++ postgresql/docker-entrypoint-initdb.d/20_restore_db.sh
@@ -3,12 +3,16 @@ set -e
 
 echo "Backup and restoring $DB_CKAN_NAME via pipe to new host."
 PGPASSWORD="$DB_PASS" pg_dump --verbose --format c \
+    --extension=plpgsql \
+    --exclude-table=package_extent --exclude-table=spatial_ref_sys \
     --host "$DB_HOST" --username "$DB_USER" "$DB_CKAN_NAME" \
     | pg_restore --verbose --exit-on-error \
     --username postgres --dbname "$DB_CKAN_NAME"
 
 echo "Backup and restoring $DB_DOI_NAME via pipe to new host."
 PGPASSWORD="$DB_PASS" pg_dump --verbose --format c \
+    --extension=plpgsql \
+    --exclude-table=package_extent --exclude-table=spatial_ref_sys \
     --host "$DB_HOST" --username "$DB_USER" "$DB_DOI_NAME" \
     | pg_restore --verbose --exit-on-error \
     --username postgres --dbname "$DB_DOI_NAME"
